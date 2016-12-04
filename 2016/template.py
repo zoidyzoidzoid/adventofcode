ca15fe8f63a37e50a5371c9cc562cc34185c60f9
William Stewart <zoidbergwill@gmail.com>
2016-12-04 11:34:59 +0000

    Add template

---
+++ 2016/template.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+import fileinput
+from collections import *
+from functools import *
+from itertools import *
+
+for line in fileinput.input():
+    line = line.strip()
+
+print(f'hello world')
