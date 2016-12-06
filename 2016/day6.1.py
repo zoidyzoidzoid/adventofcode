db087eadf81cfe886b1ee7ac12dda98e7bd545d2
William Stewart <zoidbergwill@gmail.com>
2016-12-06 05:11:19 +0000

    Day 6 2016 solutions

---
+++ 2016/day6.1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3.6
+import fileinput
+from collections import *
+from functools import *
+from itertools import *
+
+results = defaultdict(Counter)
+
+for line in fileinput.input():
+    line = line.strip()
+
+    for index, c in enumerate(line):
+        results[index][c] += 1
+
+for key in sorted(results):
+    print(results[key].most_common(1)[0][0], end='')
+
+print(f'hello world')
