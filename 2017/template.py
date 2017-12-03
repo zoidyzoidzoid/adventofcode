30377641dc629d0a74af7e17042f4eb6c99fc0d4
William Stewart <zoidbergwill@gmail.com>
2017-12-03 09:21:55 +0200

    Add math imports to template

--- 2017/template.py
+++ 2017/template.py
@@ -3,7 +3,12 @@ import fileinput
 from collections import *
 from functools import *
 from itertools import *
+from math import *
+
+
+def process(line):
+    pass
 
 for line in fileinput.input():
     line = line.strip()
-
+    print(process(line))
